4f8dccf7daef17ad3bae8d3d7fc4acaf8ace2043
0xsoon <kevinh9618@gmail.com>
2023-11-11 14:45:34 +0000

    Adds iron session to authentication

--- pages/admin.js
+++ pages/admin.js
@@ -14,7 +14,7 @@ export default function Admin(){
         setIsLoading(true);
 
         const formData = new FormData(event.target);
-        const response = await fetch('/api/auth', {
+        const response = await fetch('/api/login', {
             method: 'POST',
             headers: new Headers({'content-type': 'application/json'}),
             body: JSON.stringify(Object.fromEntries(formData.entries()))
@@ -32,8 +32,8 @@ export default function Admin(){
         <Layout isLogin>
             <h1 className="text-lg font-bold">Admin Login</h1>
             <form onSubmit={onSubmit}>
-                <label for="user" >Username</label>
-                <input name="user" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                <label for="username" >Username</label>
+                <input name="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                 <label for="password">Password</label>
                 <input name="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                 <button type="submit" disabled={isLoading} class="bg-gray-500 hover:bg-gray-700 text-white my-3 py-1 px-2 rounded">
