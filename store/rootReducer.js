c896d2f655f5d561505fde4a1ff0af7c01c50c8a
0xsoon <kevinh9618@gmail.com>
2023-11-16 14:37:43 +0000

    Removes reducers to use next js data fetching capability

--- store/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import authReducer from './authSlice'
-
-const rootReducer = combineReducers({
-    authReducer: authReducer
-})
-
-export default rootReducer;
